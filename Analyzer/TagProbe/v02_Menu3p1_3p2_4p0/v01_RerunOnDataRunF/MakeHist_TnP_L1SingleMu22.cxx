92822c58d6eac37d7ddcdfeae1794108ad8a40f8
Kyoengpil Lee <kplee@cern.ch>
2017-12-13 20:53:21 -0500

    split data

--- Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/v01_RerunOnDataRunF/MakeHist_TnP_L1SingleMu22.cxx
+++ Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/v01_RerunOnDataRunF/MakeHist_TnP_L1SingleMu22.cxx
@@ -21,16 +21,28 @@ void Setup( HistProducer* producer, TString dataType )
 		// producer->AddDataPath( dataPath+"/92X/TSntuple_v20171128_SingleMuon_Run2017Dv1_302635_to_302635_GoldenJSON_Menu3p1/ntuple_*.root");
 		// producer->AddDataPath( dataPath+"/92X/TSntuple_v20171129_SingleMuon_Run2017Dv1_302525_to_302634_GoldenJSON_Menu3p1/ntuple_*.root");
 	}
-	else if( dataType == "Menu3p2" )
+	else if( dataType == "Menu3p2_v1" )
 	{
-		producer->SetOutputFileName("ROOTFile_MakeHist_TnP_L1SingleMu22_Menu3p2.root" );
+		producer->SetOutputFileName("ROOTFile_MakeHist_TnP_L1SingleMu22_Menu3p2_v1.root" );
 
-		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171208_SingleMuon_Run2017Fv1_305832_to_305862_GoldenJSON_Menu3p2/ntuple_*.root");
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171208_SingleMuon_Run2017Fv1_305832_to_305862_GoldenJSON_Menu3p2/v1/ntuple_*.root");
 	}
-	else if( dataType == "Menu4p0" )
+	else if( dataType == "Menu3p2_v2" )
 	{
-		producer->SetOutputFileName("ROOTFile_MakeHist_TnP_L1SingleMu22_Menu4p0.root" );
+		producer->SetOutputFileName("ROOTFile_MakeHist_TnP_L1SingleMu22_Menu3p2_v2.root" );
 
-		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171208_SingleMuon_Run2017Fv1_305832_to_305862_GoldenJSON_Menu4p0/ntuple_*.root");
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171208_SingleMuon_Run2017Fv1_305832_to_305862_GoldenJSON_Menu3p2/v2/ntuple_*.root");
+	}
+	else if( dataType == "Menu4p0_v1" )
+	{
+		producer->SetOutputFileName("ROOTFile_MakeHist_TnP_L1SingleMu22_Menu4p0_v1.root" );
+
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171208_SingleMuon_Run2017Fv1_305832_to_305862_GoldenJSON_Menu4p0/v1/ntuple_*.root");
+	}
+	else if( dataType == "Menu4p0_v2" )
+	{
+		producer->SetOutputFileName("ROOTFile_MakeHist_TnP_L1SingleMu22_Menu4p0_v2.root" );
+
+		producer->AddDataPath( dataPath+"/92X/TSntuple_v20171208_SingleMuon_Run2017Fv1_305832_to_305862_GoldenJSON_Menu4p0/v2/ntuple_*.root");
 	}
 }
---
+++ Analyzer/TagProbe/v02_Menu3p1_3p2_4p0/v01_RerunOnDataRunF/script_hadd_L1SingleMu22.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+hadd ROOTFile_MakeHist_TnP_L1SingleMu22_Menu3p2.root \
+ROOTFile_MakeHist_TnP_L1SingleMu22_Menu3p2_v1.root \
+ROOTFile_MakeHist_TnP_L1SingleMu22_Menu3p2_v2.root
+
+hadd ROOTFile_MakeHist_TnP_L1SingleMu22_Menu4p0.root \
+ROOTFile_MakeHist_TnP_L1SingleMu22_Menu4p0_v1.root \
+ROOTFile_MakeHist_TnP_L1SingleMu22_Menu4p0_v2.root
+
+echo "finished"
+
